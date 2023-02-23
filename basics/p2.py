daf1a31abd2fb2bac7992063fee963214985c3fb
Parth Bhanushali <116676891+The-Parth@users.noreply.github.com>
2023-02-23 11:39:23 +0530

    Create p2.py

---
+++ basics/p2.py
@@ -0,0 +1,9 @@
+string = "Python programming"
+print(string[string.index('i')])
+print(len(string))
+print(string[0:6])
+print(string[7:14])
+print(string[2:4]+string[15:18])
+print(string.upper())
+s2 = "is interesting"
+print(string+s2)
